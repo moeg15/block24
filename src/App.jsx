b0ecd80b1c1888681227fbea4b474ad84a892317
moses <mosesgreenstein137@gmail.com>
2024-04-18 09:25:59 -0400

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-
+import { puppyList } from "./data"
+import { useState } from "react"
+import './App.css'
 
 function App() {
 
+const [puppies, setPuppies]= useState(puppyList)
+const [featPupId, setFeatPupId] = useState(null)
 
+const handleClick = (puppy) => {
+  setFeatPupId(puppy.id)
+}
+const featuredPup = puppies.find((pup)=>pup.id===featPupId)
+console.log(featuredPup)
   return (
     <>
-   
+      { puppies.map((puppy) => {
+     return <p className="blue" key={puppy.id} onClick={()=>handleClick(puppy)}> {puppy.name}</p>
+   })}
+    {featPupId && (
+        <div className="background"> 
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   )
 }
